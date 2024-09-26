6cb660273103a48d750fe494a161e16ab3f19855
LuizGuimarães <75961525+LuizGuimah@users.noreply.github.com>
2024-09-25 21:50:26 -0300

    Update PID.ino
    Need to check for overshooting and try two consecutive turns

--- scripts/PID.ino
+++ scripts/PID.ino
@@ -10,8 +10,7 @@ BluetoothSerial SerialBT;
 #define BIN2 19
 #define PWMB 21
 
-const unsigned int sensor[] = {27, 26, 25, 33, 32, 35, 15, 36};
-const unsigned int sensorLat[] = {13, 14, 34, 39};
+const unsigned int sensor[] = {14, 27, 26, 25, 33, 32, 35, 34};
 const int n_sensores = 8;
 int pesos[] = {-4, -3, -2, -1, 1, 2, 3, 4};
 
@@ -20,10 +19,10 @@ int rspeed;
 int lspeed;
 const int base_speed = 80;
 
-int pos = 0;
-int sensor_read[n_sensores];
-long sensor_average = 0;
-int sensor_sum = 0;
+float pos = 0;
+float sensor_read[n_sensores];
+float sensor_average = 0;
+float sensor_sum = 0;
 int r_read=0;
 
 float p;
@@ -34,7 +33,6 @@ float d;
 float lp;
 float error;
 float correction;
-float sp;
 
 float Kp = 2.4;
 float Ki = 0.002;
@@ -43,6 +41,9 @@ int state = 0;
 int pid_calc();
 void calc_turn();
 
+String input;
+long anterior;
+
 void setup()
 {
   SerialBT.begin("LineFollower");
@@ -50,6 +51,7 @@ void setup()
   for(int i=0; i<n_sensores; i++){
     pinMode(sensor[i], INPUT);
   }
+  pinMode(0, INPUT);
 
   //motors
   pinMode(STBY, OUTPUT);
@@ -70,11 +72,11 @@ void setup()
   
   Serial.begin(115200);
   
-  sp = 0;
   while(!SerialBT.available()) {delay(10);}
-  String input = SerialBT.readString();  // Read the incoming data as a string
+  input = SerialBT.readString();  // Read the incoming data as a string
   updatePIDConstants(input); 
-
+  
+  anterior = micros();
   analogWrite(PWMA, base_speed);
   analogWrite(PWMB, base_speed);
 }
@@ -82,30 +84,22 @@ void setup()
 
 void loop()
 {
-  //atual = micros();
-  //Serial.println(atual-anterior);
-  //anterior = atual;
   if (SerialBT.available()) {  // Check if Bluetooth data is available
-    String input = SerialBT.readString();  // Read the incoming data as a string
+    input = SerialBT.readString();  // Read the incoming data as a string
     updatePIDConstants(input);  // Function to parse and update Kp, Ki, Kd
   }
-  //boolean value
-  //1 if reading white
-  //0 if reading black
-  r_read = analogRead(39) < 3500;
+
+  r_read = analogRead(36) < 3000;
   switch (state){
     case 0:
-      calc_turn();
       if (r_read)
         state++;
       break;
     case 1:
-      calc_turn();
       if (!r_read)
         state++;
       break;
     case 2:
-      calc_turn();
       if (r_read)
         state++;
       break; 
@@ -113,30 +107,37 @@ void loop()
       analogWrite(PWMA, 0);
       analogWrite(PWMB, 0);
       while(!SerialBT.available()) {delay(10);}
-      String input = SerialBT.readString();  // Read the incoming data as a string
+      String input = SerialBT.readString();
       updatePIDConstants(input);
       state=0;
       break;
   }
-  delay(10);
+  calc_turn();
 }
 
 int pid_calc()
 {
   sensor_average = 0;
   sensor_sum = 0;
-
-  for(int i = 0; i < n_sensores; i++)
-  {
-    sensor_read[i]=analogRead(sensor[i]);
-    sensor_average += sensor_read[i]*pesos[i]*100;
-    sensor_sum += sensor_read[i];
+  while(micros() - anterior < 10000){}
+    for(int i = 0; i < n_sensores; i++)
+    {
+      sensor_read[i]=analogRead(sensor[i]);
+      sensor_average += sensor_read[i]*pesos[i];
+      sensor_sum += sensor_read[i];
+    }
+  anterior = micros();
+ 
+  pos = (sensor_average / sensor_sum)*100;
+  if(pos>1){
+    p=pos-1;
+  }
+  else{
+    if(pos<-1){
+      p = pos+1;
+    }
   }
 
-  pos = int(sensor_average / sensor_sum);
-
-  error = pos-sp;
-  p = error;
   integral += p;
   integral = constrain(integral, integral_min, integral_max);
   d = p - lp;
@@ -148,8 +149,11 @@ int pid_calc()
 void calc_turn()
 {
   correction = pid_calc();
-  rspeed = base_speed - correction;
-  lspeed = base_speed + correction;
+  rspeed = (base_speed + correction);
+  lspeed = (base_speed - correction);
+  //Serial.print(lspeed);
+  //Serial.print("  ");
+  //Serial.println(rspeed);
   
   rspeed = constrain(rspeed, 0, 255);
   lspeed = constrain(lspeed, 0, 255);
